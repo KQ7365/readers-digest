7640aacb05e4b181f93293e241db8df23452274c
Kevin Quail <kevinq.7365@gmail.com>
2023-11-10 15:01:54 -0600

    server side complete ATM

--- digestapi/models/book.py
+++ digestapi/models/book.py
@@ -5,7 +5,7 @@ class Book(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='books_created')
     title = models.CharField(max_length=155)
     author = models.CharField(max_length=155)
-    isbn = models.IntegerField()
+    isbn_number = models.IntegerField()
     cover_image_url = models.CharField(max_length=200)
     categories = models.ManyToManyField(
         "Category",
--- digestapi/views/__init__.py
+++ digestapi/views/__init__.py
@@ -1,3 +1,4 @@
 from .users import UserViewSet
 from .books import BookViewSet, BookSerializer
 from .categories import CategoryViewSet, CategorySerializer
+from .reviews import ReviewSerializer, ReviewViewSet
